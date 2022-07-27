d1566501ce3b61a078187daedf81ffccfe5927fb
Tate Taczak <s19btaczak@gmail.com>
2022-07-27 15:54:17 -0700

    Testing this feature

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
-using JuliaConSamplePackage
+using .JuliaConSamplePackage
 using Test
 
 @testset "JuliaConSamplePackage.jl" begin
     # Write your tests here.
+    @test JuliaConSamplePackage.newton(sin, cos, 1.0) ≈ 0.0
+    @test JuliaConSamplePackage.newton(cos, x-> -sin(x), 1.0) ≈ pi/2
+
+    @testset "autodiff" begin
+        @test abs(JuliaConSamplePackage.newton(sin,0.5) - 0.0) <= 100*eps(1.0)
+        @test JuliaConSamplePackage.newton(cos, 1.0) ≈ pi/2
+    end
 end
